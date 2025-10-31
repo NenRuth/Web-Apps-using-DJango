c1ad701c60a18d503da5d148f4aefd571302b530
Nen Ruth <nenruth9@gmail.com>
2025-10-31 04:08:26 +0100

    Deploying

---
+++ learning_log/urls.py
@@ -0,0 +1,14 @@
+# learning_log/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('', include('learning_logs.urls')),  # homepage routes to app
+]
+
+
+
+
+
